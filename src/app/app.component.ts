55593aa50f105050db5307380f67a4947690b21d
Gabrielyoda <yodaegan@gmail.com>
2021-06-12 12:16:16 +0700

    done app without component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'challenge-angular';
+  images = ["assets/carousel-1.png", "assets/carousel-2.png", "assets/carousel-3.png"];
+}
