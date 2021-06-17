7348b477477d7207abedb40030b17bab4ef18550
Gabrielyoda <yodaegan@gmail.com>
2021-06-17 09:00:10 +0700

    done challenge

--- src/app/shared/directives/bs-navbar.directive.ts
+++ src/app/shared/directives/bs-navbar.directive.ts
@@ -22,7 +22,7 @@ export class BsNavbarDirective {
   }
 
   @HostBinding('class')
-  applyStyles(): string {
+  get applyStyles(): string {
     return `navbar navbar-expand-lg navbar-light fixed-top ${this.defaultStyle}`
   }
 
